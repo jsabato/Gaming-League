b59ec35f98cbcf80808094c5b05991544b83a6f1
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2015-02-22 18:21:26 -0800

    added email to migration

--- db/migrate/20150223021635_create_players.rb
+++ db/migrate/20150223021635_create_players.rb
@@ -2,6 +2,7 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :playername
+      t.string :email
       t.integer :killavg
       t.integer :deathavg
       t.integer :assistavg
